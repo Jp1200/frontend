120873af383d06c363a408e01a5c53a813e9ae04
Jp1200 <jprkr96@gmail.com>
2020-11-13 09:12:34 -0600

    scrollable magic

--- comp-ui/src/App.js
+++ comp-ui/src/App.js
@@ -7,7 +7,7 @@ function App() {
     <div className="App">
       <header className="App-header">
         {/* <img src={logo} className="App-logo" alt="logo" /> */}
-        <Video video={samp}/>
+        <Video video={samp} duration={600}/>
       </header>
     </div>
   );
--- comp-ui/src/components/Video/Video.js
+++ comp-ui/src/components/Video/Video.js
@@ -1,8 +1,12 @@
 import React from 'react';
-
+import {Controller, Scene} from 'react-scrollmagic';
 function Video(props){
+  console.log(props);
   return(
     <div>
+      <Controller>
+        <Scene duration={props.duration}/>
+      </Controller>
       <video src={props.video} autoplay={true}/>
     </div>
   )
